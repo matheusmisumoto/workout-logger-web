eea9bb5aaa49427960d8ce5721a413a3af039c21
Matheus Misumoto <49180175+matheusmisumoto@users.noreply.github.com>
2024-05-31 00:43:34 -0300

    Feat: add loading screen on login
    - Use special file loading.tsx to show loading screen on login, useful
      when API needs to start up on Cloud Run (cold start) and takes a few
      moments to get ready.
    - That means that, once ready, the app response during the login process
      will be faster.

---
+++ src/app/login/loading.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+import { Logo } from "@/components/icons/Logo"
+import bgLogo from '/public/bgLogin.webp'
+
+export default function Loading() {
+    return (
+        <main className="flex flex-col items-center justify-center min-h-full relative bg-brand gap-y-12 px-6 py-12 overflow-hidden login">
+            <Image src={bgLogo} alt="Workout Logger" fill className="object-cover absolute top-0 left-0 object-center opacity-20" />
+            <div className="flex-1 flex flex-col items-center justify-end z-10">
+                <Logo fill="white" className="w-2/3 h-auto" />
+            </div>
+            <div className="w-full flex flex-col flex-1 items-center py-9">
+                <p className="text-white font-bold animate-pulse text">Iniciando API...</p>
+            </div>
+        </main>
+    )
+  }
